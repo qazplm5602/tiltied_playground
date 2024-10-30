1d2a344e81ecb840bb4a1ea3e46879a7d6cb11c5
inhae0519 <lsh266409@gmail.com>
2024-10-31 00:51:35 +0900

    fix: 판정 개선 중

--- Assets/InHae/02.Scripts/Ground.cs
+++ Assets/InHae/02.Scripts/Ground.cs
@@ -1,16 +1,41 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
-    [SerializeField] private float _rimitRot;
-    private List<MassHaveObj> _onGroundObj = new List<MassHaveObj>();
-
+    [SerializeField] private Vector3 _searchSize;
+    [SerializeField] private LayerMask _objLayer;
+    [SerializeField] private float _rotRimit;
+    [SerializeField] private float _rotTime = 1f;
+    [SerializeField] private Transform _endPoint;
+    
+    public List<MassHaveObj> _onGroundObj = new List<MassHaveObj>();
+    public Collider[] _colliders;
+    
     private float _currentRotZ;
     
     private void FixedUpdate()
     {
+        //OnFieldObjSearch();
         CalculateRot();
+    }
+
+    private void OnFieldObjSearch()
+    {
+        int size = Physics.OverlapBoxNonAlloc(transform.position, _searchSize * 0.5f, _colliders,
+            transform.rotation, _objLayer);
+
+        if (size > 0)
+        {
+            _onGroundObj = _colliders.ToList().ConvertAll(x =>
+            {
+                if (x.TryGetComponent(out MassHaveObj massHaveObj))
+                    return massHaveObj;
+                
+                return default;
+            });
+        }
     }
 
     private void CalculateRot()
@@ -43,8 +68,10 @@ public class Ground : MonoBehaviour
         else
             _currentRotZ -= rotValue;
 
-        _currentRotZ = Mathf.Clamp(_currentRotZ, -_rimitRot, _rimitRot);
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, _currentRotZ));
+        _currentRotZ = Mathf.Clamp(_currentRotZ, -_rotRimit, _rotRimit);
+        transform.rotation = Quaternion.Slerp(transform.rotation, 
+            Quaternion.Euler(new Vector3(0, 0, _currentRotZ)),
+            Time.deltaTime * _rotTime);
     }
 
     private float CalculateMass(float mass, Vector3 objPos, bool isLeft)
@@ -56,27 +83,33 @@ public class Ground : MonoBehaviour
         
         if (!isLeft)
         {
-            lerpPos = Mathf.InverseLerp(groundPos.x, groundPos.x + transform.localScale.x * 0.5f, objPos.x);
+            lerpPos = Mathf.InverseLerp(groundPos.x, groundPos.x + _endPoint.position.x, objPos.x);
             calculMass = Mathf.Lerp(0, mass, lerpPos);
         }
         else
         {
-            lerpPos = Mathf.InverseLerp(groundPos.x, groundPos.x - transform.localScale.x * 0.5f, objPos.x);
+            lerpPos = Mathf.InverseLerp(groundPos.x, groundPos.x - _endPoint.position.x, objPos.x);
             calculMass = Mathf.Lerp(0, mass, lerpPos);
         }
         
         return calculMass;
     }
-
+    
     private void OnCollisionEnter(Collision other)
     {
-        if(other.collider.TryGetComponent(out MassHaveObj massHaveObj))
-            _onGroundObj.Add(massHaveObj);
+        if(other.collider.TryGetComponent(out MassHaveObj obj))
+            _onGroundObj.Add(obj);
     }
-
+    
     private void OnCollisionExit(Collision other)
     {
-        if(other.collider.TryGetComponent(out MassHaveObj massHaveObj))
-            _onGroundObj.Remove(massHaveObj);
+        if(other.collider.TryGetComponent(out MassHaveObj obj))
+            _onGroundObj.Remove(obj);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, _searchSize);
     }
 }
--- Assets/InHae/02.Scripts/MassHaveObj.cs
+++ Assets/InHae/02.Scripts/MassHaveObj.cs
@@ -1,7 +1,28 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MassHaveObj : MonoBehaviour
 {
+    [SerializeField] private bool _isMove;
     [SerializeField] private float _mass;
+    [SerializeField] private float _speed;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(!_isMove)
+            return;
+        
+        float x = Input.GetAxisRaw("Horizontal");
+        _rigidbody.linearVelocity= new Vector3(x * _speed, _rigidbody.linearVelocity.y, 0);
+    }
+
     public float GetMass() => _mass;
 }
