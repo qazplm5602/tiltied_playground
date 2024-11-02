5ea3cfe637e36c5d7cde46d9470fe716b3486069
qazplm5602 <t01048187045@gmail.com>
2024-11-03 06:29:24 +0900

    feat: ball kick simulate finish

--- Assets/Domi/Scripts/BallGoalSimulateManager.cs
+++ Assets/Domi/Scripts/BallGoalSimulateManager.cs
@@ -15,11 +15,13 @@ struct BallArea {
 
 public class BallGoalSimulateManager : MonoBehaviour
 {
-    [SerializeField] int loopAmount = 30; // 많을수록 더 먼곳이어도 감지가 가능함
-    [SerializeField] float gravity;
+    [SerializeField] private int loopAmount = 30; // 많을수록 더 먼곳이어도 감지가 가능함
+    [SerializeField] private float gravity;
+    [SerializeField] private LayerMask whatIsObstacle;
+    [SerializeField] private Collider point; // 이걸로 공이 충돌 하는지 체크함
     [SerializeField] private BallArea[] areas;
     
-    private Dictionary<BallAreaType, Action> callbacks;
+    private Dictionary<BallAreaType, Action> callbacks = new();
 
     
     public void RegisterCallback(BallAreaType type, Action cb) {
@@ -35,6 +37,11 @@ public class BallGoalSimulateManager : MonoBehaviour
             callbacks.Remove(type);
     }
 
+    private void SendCallback(BallAreaType type) {
+        if (callbacks.TryGetValue(type, out Action cb))
+            cb?.Invoke();
+    }
+
     public void SimulateBall(Vector3 ballPos, Vector3 kickDir /* 힘도 있음 */) {
         float between = Vector3.Distance(GetPos(ballPos, kickDir, 0), GetPos(ballPos, kickDir, 0.1f));
         
@@ -42,10 +49,21 @@ public class BallGoalSimulateManager : MonoBehaviour
         for (int i = 1; i < loopAmount; i++)
         {
             float nowTime = i / between;
-            print(nowTime);
             Vector3 pos = GetPos(ballPos, kickDir, nowTime);
-            
+
+            // 벌래
             Debug.DrawLine(lastPos, pos, Color.red, 10f);
+
+            // 골대에 들어감???
+            if (FindGoalPost(pos, out BallAreaType detectArea)) {
+                Debug.Log($"Detect GoalPos {detectArea.ToString()}");
+                SendCallback(detectArea);
+                break;
+            }
+
+            // 어디 맞아서 더 이상 안해도 됨
+            if (HitObstacle(lastPos, pos))
+                break;
 
             lastPos = pos;
         }
@@ -56,5 +74,35 @@ public class BallGoalSimulateManager : MonoBehaviour
         float y = ballPos.y + kickDir.y * time - ((gravity * Mathf.Pow(time, 2)) / 2f);
         float z = ballPos.z + kickDir.z * time;
         return new Vector3(x, y, z);
+    }
+    
+    private RaycastHit[] hitResults = new RaycastHit[1];
+    private bool HitObstacle(Vector3 start, Vector3 end) {
+        Vector3 dir = (end - start).normalized;
+        float distance = Vector3.Distance(start, end);
+
+        int count = Physics.RaycastNonAlloc(start, dir, hitResults, distance, whatIsObstacle);
+        return count > 0;
+    }
+
+    private bool FindGoalPost(Vector3 pos, out BallAreaType type) {
+        point.enabled = false; // collider 수동 업뎃
+        point.transform.position = pos;
+        point.enabled = true;
+
+        // point.GetPositionAndRotation(out Vector3 asdf, out var _);
+        // Debug.Log($"point coords: {asdf}");
+        
+        foreach (var value in areas) {
+            bool result = value.area.HasPointIn();
+
+            if (result) {
+                type = value.type;
+                return true;
+            }
+        }
+
+        type = default;
+        return false;
     }
 }
