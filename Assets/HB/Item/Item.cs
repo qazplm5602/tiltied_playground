3cfcfb2c4f070b0187b1a90c5ffe42195524b1e4
hanbyul0103 <all007b@naver.com>
2024-11-03 10:50:32 +0900

    fix: remove auto definition system

--- Assets/HB/Item/Item.cs
+++ Assets/HB/Item/Item.cs
@@ -1,25 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.LightTransport;
 
 public class Item : MonoBehaviour
 {
     [SerializeField] private ItemSO[] _itemSO;
     public int buffTime;
-
-    private string objName = "";
-    [SerializeField] private List<string> nameList = new();
-
-    private void OnEnable()
-    {
-        objName = gameObject.name;
-
-        nameList = objName.Split("/").ToList();
-
-        buffTime = int.Parse(nameList[nameList.Count - 1]);
-    }
 
     private IEnumerator GetItem(Player player)
     {
@@ -44,6 +29,7 @@ public class Item : MonoBehaviour
             if (other.TryGetComponent<Player>(out Player player))
             {
                 StartCoroutine(GetItem(player));
+                gameObject.SetActive(false);
             }
         }
     }
