574d2508d9d1b3f1217b0d2495b58dd5f655060a
qazplm5602 <t01048187045@gmail.com>
2024-11-05 01:16:19 +0900

    feat: managerManager

--- Assets/Domi/Scripts/TestDomi.cs
+++ Assets/Domi/Scripts/TestDomi.cs
@@ -4,34 +4,34 @@ public class TestDomi : MonoBehaviour
 {
     [SerializeField] PlayerControlSO controls;
 
-    private void Awake() {
-        controls.ItemUseEvent += OnItemUse;
-        controls.SkillEvent += OnSkill;
-        controls.InteractEvent += OnInteract;
-    }
+    // private void Awake() {
+    //     controls.ItemUseEvent += OnItemUse;
+    //     controls.SkillEvent += OnSkill;
+    //     controls.InteractEvent += OnInteract;
+    // }
 
-    private void OnItemUse() {
-        print("OnItemUse!");
-    }
+    // private void OnItemUse() {
+    //     print("OnItemUse!");
+    // }
 
-    private void OnSkill() {
-        print("OnSkill!");
-    }
+    // private void OnSkill() {
+    //     print("OnSkill!");
+    // }
 
-    private void OnInteract(bool isDown) {
-        print($"OnInteract {isDown}");
-    }
+    // private void OnInteract(bool isDown) {
+    //     print($"OnInteract {isDown}");
+    // }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        // controls.StartBindChange();
-    }
+    // // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+    //     // controls.StartBindChange();
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 pos = controls.GetMoveDirection();
-        // print(pos);
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     Vector2 pos = controls.GetMoveDirection();
+    //     // print(pos);
+    // }
 }
