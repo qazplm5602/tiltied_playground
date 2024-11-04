454fd663758b1fafcb5ded1319f834ee964b29c7
qazplm5602 <t01048187045@gmail.com>
2024-11-04 17:54:39 +0900

    feat: fadeout transition

--- Assets/Domi/Scripts/CameraTransition.cs
+++ Assets/Domi/Scripts/CameraTransition.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using DG.Tweening;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,13 +7,13 @@ using UnityEngine.UI;
 
 public class CameraTransition : MonoBehaviour
 {
-    [SerializeField] private int basePriority; // 기본 순위
-    [SerializeField] private int topPriority; // 보일떄 순위
+    [SerializeField] private float fadeOutDuration = 0.3f;
     [SerializeField] private string screenName = "CamScreen";
 
     private RenderTexture renderTexture = null;
     private RawImage screen;
     private CameraType currentCamType = CameraType.Main;
+    private Sequence process;
     
     private void Awake() {
         SceneManager.sceneLoaded += OnSceneLoad;
@@ -30,6 +31,10 @@ public class CameraTransition : MonoBehaviour
     }
 
     public void FadeChangeCam(CameraType cam) {
+        if (process != null) {
+            process.Kill(true);
+        }
+
         // Screen.width
         if (renderTexture == null) {
             CreateRenderTexture();
@@ -37,14 +42,28 @@ public class CameraTransition : MonoBehaviour
 
         Camera oldCam = CameraManager.Instance.GetCamera(currentCamType);
         // oldCam.targetDisplay = 2; // 렌더 텍스쳐 적용하면 필요 없어짐 ㅎㅎ
-        oldCam.targetTexture = renderTexture;
 
         Camera nowCam = CameraManager.Instance.GetCamera(cam);
-        nowCam.targetDisplay = 0;
 
         // 활성화
         screen.gameObject.SetActive(true);
         screen.color = Color.white;
+
+        // 애님
+        process = DOTween.Sequence();
+        process.Append(screen.DOFade(0, fadeOutDuration)).SetEase(Ease.Linear);
+        process.OnStart(() => {
+            oldCam.targetTexture = renderTexture;
+            nowCam.targetDisplay = 0;
+        });
+        process.OnComplete(() => {
+            oldCam.targetDisplay = 2;
+            oldCam.targetTexture = null;
+            screen.gameObject.SetActive(false);
+            process = null;
+        });
+
+        currentCamType = cam;
     }
 
     private void CreateRenderTexture() {
