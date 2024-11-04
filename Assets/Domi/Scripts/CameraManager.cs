c46f143bc46e1d6491407e36e5cf52b9817341fe
qazplm5602 <t01048187045@gmail.com>
2024-11-04 15:17:23 +0900

    feat: live camera load

--- Assets/Domi/Scripts/CameraManager.cs
+++ Assets/Domi/Scripts/CameraManager.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum CameraType {
     Main,
@@ -17,10 +20,36 @@ public struct CameraData {
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField] CameraData[] cameraList;
+    // [SerializeField] CameraData[] cameraList;
+    Dictionary<CameraType, CinemachineCamera> cameraList;
     [field: SerializeField] public CameraTransition Transition { get; private set; }
 
     private void Awake() {
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+
+    private void OnDestroy() {
+        SceneManager.sceneLoaded -= OnSceneLoad;
+    }
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode) {
+        cameraList = new(); // 다시 파
         
+        CinemachineCamera[] cameras = FindObjectsByType<CinemachineCamera>(FindObjectsSortMode.None);
+        print($"find camera length: {cameras.Length}");
+        
+        foreach (var item in cameras)
+        {
+            string entityName = item.gameObject.name;
+            try {
+                CameraType type = (CameraType)Enum.Parse(typeof(CameraType), entityName);
+                cameraList[type] = item;
+            } catch (ArgumentException) {
+                // 그냥 다른 카메라 인가봄
+            }
+            
+        }
+
+        print($"camera insert count: {cameraList.Count}");
     }
 }
