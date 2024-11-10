f1a86e7460ec79307f1fcd6d47cd6826baebcd69
hanbyul0103 <all007b@naver.com>
2024-11-10 22:15:25 +0900

    feat: skill board

--- Assets/HB/01.Scripts/UI/SkillBoard.cs
+++ Assets/HB/01.Scripts/UI/SkillBoard.cs
@@ -1,12 +1,18 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SkillBoard : MonoBehaviour
 {
-    [SerializeField] private Image _coolTimeImage;
+    [SerializeField] private Skill _skill;
 
-    private void CoolDownImage(Image coolDownImage)
+    private void Start()
     {
+        CoolDown(_skill.skillImages[1]);
+    }
 
+    private void CoolDown(Image skillImage)
+    {
+        skillImage.DOFillAmount(1, _skill.coolTime);
     }
 }
