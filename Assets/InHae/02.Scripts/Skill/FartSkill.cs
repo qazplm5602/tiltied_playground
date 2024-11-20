4407e01e40e97086b30f9561b9e9459ea43c965a
qazplm5602 <t01048187045@gmail.com>
2024-11-20 23:53:48 +0900

    feat: fart sound

--- Assets/InHae/02.Scripts/Skill/FartSkill.cs
+++ Assets/InHae/02.Scripts/Skill/FartSkill.cs
@@ -5,6 +5,7 @@ public class FartSkill : SkillBase
 {
     [SerializeField] private GameObject fartEffectPrefab;
     [SerializeField] private GameObject massPrefab;
+    [SerializeField] private SoundSO fartSound;
 
     public override void UseSkill()
     {
@@ -13,6 +14,9 @@ public class FartSkill : SkillBase
         fartTrm.forward = -transform.forward; // 플레이어 반대 방향
         
         StartCoroutine(NextFrameRemove(Instantiate(massPrefab, transform.position, Quaternion.identity)));
+
+        // 소리
+        SoundManager.Instance.PlaySFX(Vector3.zero, fartSound);
     }
 
     private IEnumerator NextFrameRemove(GameObject entity) {
