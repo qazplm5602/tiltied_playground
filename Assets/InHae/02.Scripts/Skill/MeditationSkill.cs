fb28e6a49799ce34c25b2d04dd341ba6b853d5ec
qazplm5602 <t01048187045@gmail.com>
2024-11-21 01:50:41 +0900

    feat: zhonya effect

--- Assets/InHae/02.Scripts/Skill/MeditationSkill.cs
+++ Assets/InHae/02.Scripts/Skill/MeditationSkill.cs
@@ -1,13 +1,22 @@
 using DG.Tweening.Core.Easing;
 using System.Collections;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class MeditationSkill : SkillBase
 {
     [SerializeField] private float _meditationTime = 5.0f;
+    [SerializeField] private Material _mat;
+    [SerializeField] private ParticleSystem _particlePrefab;
+    
     private Player _otherPlayer;
     private GroundTiltied _ground;
+    private SkinnedMeshRenderer[] _renderers;
+
+    private void Awake() {
+        _renderers = GetComponentsInChildren<SkinnedMeshRenderer>();
+    }
 
     private void Start()
     {
@@ -22,13 +31,30 @@ public class MeditationSkill : SkillBase
     {
         StartCoroutine(Meditation());
     }
-
+    
     private IEnumerator Meditation()
     {
         _ground.enabled = false;
         _otherPlayer.IsMeditation = true;
+        
+        foreach (var item in _renderers)
+        {
+            List<Material> mats = item.materials.ToListPooled();
+            item.materials = mats.ToArray();
+        }
+
+        // 이펙트
+        Instantiate(_particlePrefab, transform.position, Quaternion.identity);
 
         yield return new WaitForSeconds(_meditationTime);
+
+        foreach (var item in _renderers)
+        {
+            List<Material> mats = item.materials.ToListPooled();
+            mats.Remove(_mat);
+
+            item.materials = mats.ToArray();
+        }
 
         _ground.enabled = true;
         _otherPlayer.IsMeditation = false;
