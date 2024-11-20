19e28f706e09b743f839628b36e12dafb71785c3
qazplm5602 <t01048187045@gmail.com>
2024-11-20 19:35:10 +0900

    feat: fart skill

--- Assets/InHae/02.Scripts/Skill/FartSkill.cs
+++ Assets/InHae/02.Scripts/Skill/FartSkill.cs
@@ -1,9 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 public class FartSkill : SkillBase
 {
+    [SerializeField] private GameObject fartEffectPrefab;
+    [SerializeField] private GameObject massPrefab;
+
     public override void UseSkill()
     {
-        Debug.Log("방귀");
+        Transform fartTrm = Instantiate(fartEffectPrefab).transform;
+        fartTrm.position = transform.position;
+        fartTrm.forward = -transform.forward; // 플레이어 반대 방향
+        
+        StartCoroutine(NextFrameRemove(Instantiate(massPrefab, transform.position, Quaternion.identity)));
+    }
+
+    private IEnumerator NextFrameRemove(GameObject entity) {
+        yield return new WaitForFixedUpdate();
+        Destroy(entity);
     }
 }
