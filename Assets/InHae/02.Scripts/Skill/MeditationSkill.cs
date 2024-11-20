02b1313d0b53cca7d643ab3c6402934d4c897507
hanbyul0103 <all007b@naver.com>
2024-11-21 00:26:06 +0900

    feat: meditation

--- Assets/InHae/02.Scripts/Skill/MeditationSkill.cs
+++ Assets/InHae/02.Scripts/Skill/MeditationSkill.cs
@@ -1,9 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class MeditationSkill : SkillBase
 {
+    [SerializeField] private float _meditationTime = 5.0f;
+    private Player _otherPlayer;
+    private GroundTiltied _ground;
+
+    private void Start()
+    {
+        Player bluePlayer = ManagerManager.GetManager<PlayerManager>().GetPlayer(BallAreaType.Blue);
+        Player redPlayer = ManagerManager.GetManager<PlayerManager>().GetPlayer(BallAreaType.Red);
+        _ground = FindAnyObjectByType<GroundTiltied>();
+
+        _otherPlayer = bluePlayer == _player ? redPlayer : bluePlayer;
+    }
+
     public override void UseSkill()
     {
-        Debug.Log("명상");
+        StartCoroutine(Meditation());
+    }
+
+    private IEnumerator Meditation()
+    {
+        _otherPlayer.IsMeditation = _ground.enabled = false;
+
+        yield return new WaitForSeconds(_meditationTime);
+
+        _otherPlayer.IsMeditation = _ground.enabled = true;
     }
 }
