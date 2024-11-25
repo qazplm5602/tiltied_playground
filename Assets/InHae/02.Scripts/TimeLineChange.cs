0787c478dd14360bcc76e305bae3769d5659fba4
inhae0519 <lsh266409@gmail.com>
2024-11-25 15:31:32 +0900

    feat : main cutscene

---
+++ Assets/InHae/02.Scripts/TimeLineChange.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimeLineChange : MonoBehaviour
+{
+    [SerializeField] private List<TimelineAsset> _timelineList;
+
+    private PlayableDirector _playableDirector;
+
+    private int _timeLineIdx;
+
+    private void Awake()
+    {
+        _playableDirector = GetComponent<PlayableDirector>();
+    }
+
+    public void ChangeTrack()
+    {
+        _timeLineIdx++;
+        if (_timeLineIdx >= _timelineList.Count)
+            _timeLineIdx = 0;
+        
+        _playableDirector.playableAsset = _timelineList[_timeLineIdx];
+        _playableDirector.Play();
+    }
+}
